e9f72f37adae058e91cf128cd3aa6ffc4a31f129
Maksym Murtazin <max.murtazin2006@gmail.com>
2026-06-26 16:52:10 +0300

    Created two packages with some modules. Imported all modules in main and defined cinema_visit method that manages the cinema.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-# write your imports here
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    customers_instances = []
+    for customer in customers:
+        customers_instances.append(Customer(customer["name"],
+                                            customer["food"]))
+        CinemaBar.sell_product(customer["food"], customers_instances[-1])
+    cleaner_instance = Cleaner(cleaner)
+    hall_instance = CinemaHall(hall_number)
+    CinemaHall.movie_session(
+        hall_instance,
+        movie,
+        customers_instances,
+        cleaner_instance
+    )
